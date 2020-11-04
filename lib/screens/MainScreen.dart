0eccd6dfc1ecff68de197ad96fe8ae8386a1c524
Vladimir <wwwovilon@gmail.com>
2020-11-04 22:42:40 +0200

    top and bottom curves

--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -54,14 +54,76 @@ class MenuButtonState extends State<MenuButton>{
 class BackGround extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(gradient: LinearGradient(
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-        colors: [ResColor.BDGrad1, ResColor.BDGrad2],
-        stops: [0.8, 1]
-      )),
+    return Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [ResColor.BDGrad1, ResColor.BDGrad2],
+              stops: [0.8, 1]
+          )),
+        ),
+        Container(width: double.infinity, height: 100,
+          child: CustomPaint(
+            painter: TopCurve(),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(width: double.infinity, height: 50,
+            child: CustomPaint(painter: BottomCurve(),),),)
+      ]
     );
+  }
+}
+
+class TopCurve extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = Colors.white;
+    paint.style = PaintingStyle.fill;
+
+    var path = Path();
+
+    path.moveTo(0, size.height * 0.5);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 1,
+        size.width * 0.5, size.height * 0.6);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.4,
+        size.width * 1.0, size.height * 0.5);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class BottomCurve extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = Colors.white;
+    paint.style = PaintingStyle.fill;
+
+    var path = Path();
+
+    path.moveTo(0, size.height * 0.8);
+    path.quadraticBezierTo(size.width * 0.2, size.height * 0.5,
+        size.width * 0.4, size.height * 1);
+    path.lineTo(0, size.height);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
 
